3b6d847584e22f924b1a61c0a4376925e48d07e5
Haferkorn <anette.haferkorn@web.de>
2021-05-18 20:42:28 +0200

    Update censor_dispenser.py
    Added possibility to input the sensible information

--- projects/censor_dispenser.py
+++ projects/censor_dispenser.py
@@ -2,34 +2,35 @@ from pathlib import Path
 import PyPDF2
 import docx
 
+#Access the PDF File you want to censor
 file = open('Haferkorn.pdf', 'rb')
 pdf = PyPDF2.PdfFileReader(file)
-
+#Change this line if you want to censor information NOT on the first page
 first_page = pdf.getPage(0)
-pdf_path = (
-    Path.home()
-    / "Haferkorn.pdf"
-    / "censor_dispenser.py"
-)
-outputpath=Path.home()/"Anette.txt"
 
+#Store your Text
 page_text=first_page.extractText()
 
 
-def censsor(phrase,sensible_content_list):
+sensible_information=input("Please enter all the Information you want to censor (SEPTERATED with ,")
+def clean_data(sensible_information_string):
+    sensible_data=sensible_information_string.split(",")
+    return sensible_data
+
+def censor(phrase,sensible_content_list):
     for sensible_content in sensible_content_list:
         if sensible_content in phrase:
-            phrase=phrase.replace(sensible_content, "CENSSORED")      
+            phrase=phrase.replace(sensible_content, "CENSORED")      
     return write_in_doc(phrase)
 
 def write_in_doc(phrase):
-    file_format=input("In which format do you want to store your information: txt or docx")
+    file_format="docx"#input("In which format do you want to store your information: txt or docx")
     if file_format=="txt":
         with open("Anette.txt", "w") as text_file:
             text_file.write(phrase)
     elif file_format=="docx":
         mydoc = docx.Document()
         mydoc.add_paragraph(phrase)
-        mydoc.save("Anette_H.docx") 
+        mydoc.save("Anette_H.docx")   
 
-print(censsor(page_text, ["Haferkorn","Anette", "28.02.1997","Stuttgart"]))
+print(censor(page_text, clean_data(sensible_information)))
